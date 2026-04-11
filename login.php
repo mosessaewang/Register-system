94356832a7c242b2da4c948b3660768e2629c944
moses <mosessaewang123@gmail.com>
2026-04-11 10:05:58 +0700

    Update login and register pages, switch to style_r.css

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_t.css">
+    <link rel="stylesheet" href="style_r.css">
     <title>Document</title>
 </head>
 <body>
--- register.php
+++ register.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style_t.css">
+    <link rel="stylesheet" href="style_r.css">
     <title>Document</title>
 </head>
 <body>
@@ -29,7 +29,7 @@
                     <div class="btn">
                         <button type="submit">submit</button>
                     </div>
-                    <p class="pa">Go to the page Login<a href="login.php">Sign in</a></p>
+                    <p class="pa">Have an account?<a href="login.php">Login here</a></p>
             </div>
         </form>
     </div>
