a543bf09e2fa03bbaacdd67be3b5ae2c9f61baac
moses <mosessaewang123@gmail.com>
2026-04-11 15:17:03 +0700

    แก้ไขตรง email ให้ต้องมีตัวเลยและลงท้ายด้วย gmail.com

--- register_r.php
+++ register_r.php
@@ -2,9 +2,18 @@
     session_start();
     include("connect_db.php");
 
+    
     $r_user = $_POST['r_user'];
     $r_email = $_POST['r_email'];
     $r_password = $_POST['r_password'];
+
+    if(!preg_match("/^(?=.*[0-9])[a-zA-Z0-9._%+-]+@gmail\.com$/", $r_email)) {
+        echo "<script>
+                alert('อีเมลต้องมีตัวเลขและใช้ @gmail.com เท่านั้น');
+                window.location = 'register.php';
+              </script>";
+        exit();
+    }
 
     $sql = "INSERT INTO tb_register(r_user, r_email, r_password)
             VALUES('$r_user', '$r_email', '$r_password')";
